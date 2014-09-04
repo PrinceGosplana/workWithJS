41120a32141e187d7780f07428f76a69307eec97
Oleksandr Isaiev <nevkluchat@gmail.com>
2014-09-04 20:20:59 +0300

    Initial Commit

---
+++ workWithJS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  workWithJS
+//
+//  Created by Oleksandr Isaiev on 04.09.14.
+//  Copyright (c) 2014 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ workWithJS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  workWithJS
+//
+//  Created by Oleksandr Isaiev on 04.09.14.
+//  Copyright (c) 2014 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
